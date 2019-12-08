524dff3b1f05bb3e903a76ebf8c44887e051e30c
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-07 16:49:44 -0800

    first commit. Initial plop config

---
+++ plopfile.js
@@ -0,0 +1,28 @@
+module.exports = plop => {
+	plop.setGenerator('generator-lambda-netlify', {
+		description: 'Plop generator for lambda functions deployed to Netlify',
+		prompts: [
+			{
+				type: 'input',
+				name: 'project-name',
+				message: 'Give a name for the project',
+				validate: input => {
+					if (/.+/.test(input)) return true
+					return 'Project name is required'
+				}
+			},
+			{
+				type: 'input',
+				name: 'project-description',
+				message: 'Give a description for the project',
+			}
+		],
+		actions: [
+			{
+				type: 'add',
+				path: '.gitignore',
+				templateFile: 'node_modules/@ziro/generator/templates/.gitignore'
+			}
+		]
+	})
+}

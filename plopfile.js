b697a723423c1dc3ad566a08fba67fd4cf3b2327
vitorbarbosa19 <vitorbarbosa19@gmail.com>
2019-12-08 15:44:31 -0800

    add actions and prompts to plopfile

--- plopfile.js
+++ plopfile.js
@@ -4,17 +4,35 @@ module.exports = plop => {
 		prompts: [
 			{
 				type: 'input',
-				name: 'project-name',
-				message: 'Give a name for the project',
+				name: 'name',
+				message: 'Give a name for the project. Will be used in package.json',
 				validate: input => {
 					if (/.+/.test(input)) return true
-					return 'Project name is required'
+					return 'Name is required'
 				}
 			},
 			{
 				type: 'input',
-				name: 'project-description',
-				message: 'Give a description for the project',
+				name: 'description',
+				message: 'Give a description for the project. Will be used in package.json',
+			},
+			{
+				type: 'input',
+				name: 'repository',
+				message: 'Paste the github repository link. Will be used in package.json',
+				validate: input => {
+					if (/.+/.test(input)) return true
+					return 'Repository is required'
+				}
+			},
+			{
+				type: 'input',
+				name: 'author',
+				message: 'Name the author of the project. Will be used in package.json',
+				validate: input => {
+					if (/.+/.test(input)) return true
+					return 'Author is required'
+				}
 			}
 		],
 		actions: [
@@ -22,7 +40,22 @@ module.exports = plop => {
 				type: 'add',
 				path: '.gitignore',
 				templateFile: 'node_modules/@ziro/generator/templates/.##gitignore##'
-			}
+			},
+			{
+				type: 'add',
+				path: 'netlify.toml',
+				templateFile: 'node_modules/@ziro/generator/templates/netlify.toml'
+			},
+			{
+				type: 'add',
+				path: 'package.json',
+				templateFile: 'node_modules/@ziro/generator/templates/package.json'
+			},
+			{
+				type: 'add',
+				path: 'webpack.functions.js',
+				templateFile: 'node_modules/@ziro/generator/templates/webpack.functions.js'
+			},
 		]
 	})
 }

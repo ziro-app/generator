bd05ab365b6a017836d0fe6dfc08715d7369b4ad
Vitor Barbosa <vitorbarbosa19@gmail.com>
2020-02-02 10:26:07 -0800

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
 
-const spawn = require('child_process').spawn
-const result = spawn('plop', [''], { shell: true, stdio: 'inherit' })
-result.stdout.on('data', data => data)
+const { spawn } = require('child_process')
+spawn('plop', [''], { shell: true, stdio: 'inherit' })

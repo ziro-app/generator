d73fce5a7d616596bd0753a987e2c7df65f71e91
Vitor Barbosa <vitorbarbosa19@gmail.com>
2020-02-02 10:19:34 -0800

    Update generator.js

--- generator.js
+++ generator.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 
 const spawn = require('child_process').spawn
-const result = spawn('node', ["./index.js"])
-result.stdout.on('data', data => console.log(data))
+const result = spawn('node', ['index.js'])
+result.stdout.on('data', data => data)
